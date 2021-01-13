b8c7ade6fe33f6ac4cb96fc2a39607a03dc4846a
алина человечкова <alinasmart19@mail.ru>
2021-01-13 13:19:04 +0600

    Добавьте файлы проекта.

---
+++ ConsoleMenu/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleMenu
+{
+    public class Menu
+    {
+        string[] items;
+
+        public Menu() { }
+        public Menu(string[] items)
+        {
+            this.items = items;
+        }
+
+
+        public void Input(string[] items)
+        {
+            this.items = items;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("\n\n\n\t\t-----------Меню--------------");
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"\t\t{i + 1}. {items[i]}");
+            }
+            Console.Write("\n\t -->");
+        }
+    }
+}
